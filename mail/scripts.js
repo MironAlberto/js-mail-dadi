9a3b4ea2789c352f70e863ab97586285b68d9ae0
MironAlberto <alberto.miron19@gmail.com>
2023-12-10 16:56:29 +0100

    completato esercizio mail

---
+++ mail/scripts.js
@@ -0,0 +1,33 @@
+let Emails = prompt("Inserisci la tua email");
+
+
+const validEmails = [
+    'paolo@a.b',
+    'carlo@a.b',
+    'luca@a.b',
+    'mario@a.b',
+    'gino@a.b',
+    'alessio@a.b',
+    'chiara@a.b',
+    'giulia@a.b',
+    'elisa@a.b',
+    'alberto@a.b'
+]
+
+let checkEmail = 0;
+
+for (i = 0; i<validEmails.length; i++){
+
+    if (validEmails[i] == Emails){
+        alert("Email valida");
+
+        checkEmail = 1;
+
+        i = validEmails.length;
+    }
+}
+
+if (checkEmail == 0){
+    alert("Email non valida");
+}
+
